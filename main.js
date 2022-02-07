1c516b9eb2b7a2e732ed329d6158b37059ba008c
Burak Doğan <76884187+burakdogangazi@users.noreply.github.com>
2022-02-08 02:43:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -144,8 +144,3 @@ function requestVideoPlaylist(playlistId){
 }
 
 
-// <div class="row">
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>           
-// </div>
-
-
